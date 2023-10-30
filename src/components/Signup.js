1623a40b3917de3dec5f9748a40c5c896fd27970
imshamshad4123 <148256600+imshamshad4123@users.noreply.github.com>
2023-10-30 20:47:45 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,7 +7,7 @@ const Signup = (props) => {
     const handleSubmit= async (e)=>{
       const {name,email,password}=credentials;
         e.preventDefault();
-        const response=await fetch(`http://localhost:5000/api/auth/createuser`,{
+        const response=await fetch(`https://notesappbackend3-fn9c.onrender.com/api/auth/createuser`,{
             method:'POST',
             headers:{
                 'content-type':'application/json',
