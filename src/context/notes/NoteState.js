d205b797840f0504134e31c270baf0729ec0e2d6
imshamshad4123 <148256600+imshamshad4123@users.noreply.github.com>
2023-10-31 01:29:27 +0530

    Update NoteState.js

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const host = "https://notesappbackend3-fn9c.onrender.com/"
+    const host = "https://notesappbackend3-fn9c.onrender.com"
     const notesInitial = []
     const [notes, setNotes] = useState(notesInitial);
     const getNotes = async () => {
